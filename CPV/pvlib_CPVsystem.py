b153cbeb806da9f7805a3bd8b939e6161bd19675
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-06-18 15:17:49 +0200

    corrections and addition on loss function and utilization factor

--- CPV/pvlib_CPVsystem.py
+++ CPV/pvlib_CPVsystem.py
@@ -1,27 +1,77 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 
-
-def optical_transmission_losses(dni, aoi):
+def optical_transmission_losses(aoi):
 
     '''
-    reduces the dni due to optical transmission losses by the lense
+    reduces the dni due to optical transmission losses by the lens
 
-    :param numeric: dni at a certain time step
-    :param numeric: aoi at a certain time step
+    :param numeric: dni at a certain timestep
+    :param numeric: aoi at a certain timestep
     :return: float, new dni minus optical tranmission losses
     '''
+
+
+
     c0=4.54545455e-09
     c1=-2.21212121e-06
     c2=8.09090909e-05
     c3=-2.68463203e-03
     c4=8.62948052e-01
 
+    if aoi >=60:
+        return 0
+    else:
+        ot= c4 + c3* aoi + c2*aoi**2+ c1*aoi**3 + c0 *aoi**4
+        return ot
 
-    dni_ot= dni * (c4 + c3* aoi + c2*aoi**2+ c1*aoi**3 + c0 *aoi**4)
-    return dni_ot
 
+def glass_transmission_losses(aoi): #todo: find mistake for aoi=59°
 
-def UF_corrected_DNI(c1, c2, c3, am, t_ambient, dni):
+    # this is an insolight specific anti-reflection coating
+    glass_ar_offset = 0.015
+    n1 = 1.0
+    n2 = 1.5
+    aoi=150
+
+    Rs = abs((n1 * math.cos(math.radians(aoi)) - n2 * np.sqrt(1-((n1/n2) * np.sin(math.radians(aoi)))**2.0))/
+             (n1 * np.cos(math.radians(aoi)) + n2 * np.sqrt(1-((n1/n2) * np.sin(math.radians(aoi)))**2.0)))**2
+
+    Rp= abs((n1 * np.sqrt(1 - ((n1/n2) * np.sin(math.radians(aoi)))**2.0) - n2 * np.cos(math.radians(aoi)))/
+            (n1 * np.sqrt(1 - ((n1/n2) * np.sin(math.radians(aoi)))**2.0) + n2 * np.cos(math.radians(aoi))))**2
+
+    Reff= 0.5 *(Rs + Rp)
+    glass_transmission = (1.0 - Reff + glass_ar_offset)**2
+    return Reff
+
+def ufam(am):
+
+    a0 =1.0015288
+    a1=-0.00878182
+    a2=-0.00146694
+
+    return a0+a1*am + a2*am**2
+
+def ufdni(dni):
+
+    d0=-2.22040018e-01
+    d1=3.08399124e-03
+    d2=-1.95350121e-06
+
+    return d0 + d1 * dni + d2 * dni ** 2
+
+def uftamb(t_ambient):
+
+    t0=8.43234532e-01
+    t1=1.04128138e-02
+    t2=-1.73841969e-04
+
+    return t0 + t1 * t_ambient + t2 * t_ambient ** 2
+
+def UF_corrected_DNI(am, t_ambient, dni):
 
     '''
     The approach follows the model of Gerstmaier (Quelle), promoting a Utilization Faktor
@@ -35,37 +85,33 @@ def UF_corrected_DNI(c1, c2, c3, am, t_ambient, dni):
             cells
     '''
 
-    ufam=ufam(am)
-    ufdni(dni)
-    uftamb(t_ambient)
+    uf_am=ufam(am)
+    uf_dni=ufdni(dni)
+    uf_tamb=uftamb(t_ambient)
+    c1=1
+    c2=1
+    c3=1
 
-    UF = c1 * ufam + c2 * ufdni + c3 * uftamb
+    df_new = pd.concat([uf_am, uf_dni, uf_tamb], axis=1)
+    df=df_new.sum(axis=1)
 
-    return dni * UF
+#    UF = c1 * uf_am + c2 * uf_dni + c3 * uf_tamb
 
-def ufam(am):
+    return dni * df
 
-    a0 = 2.54991088e-03
-    a1 = 6.08723215e-04
-    a2 = -1.27238047e-04
-    a3 = 8.42391410e-06
+if __name__ == "__main__":
 
-    return a0+a1*am + a2*am**2 + a3*am**3
+    aoi_list={}
 
-def ufdni(dni):
+    aoi=1
+    while aoi < 200:
+        gt=glass_transmission_losses(aoi)
+        aoi_list[aoi]=gt
+        aoi=aoi+1
 
-    d0=-7.05120964e-03
-    d1=4.02286998e-05
-    d2=-5.01069512e-08
-    d3=1.98629580e-11
 
-    return d0 + d1 * am + d2 * am ** 2 + d3 * am ** 3
-
-def uftamb(t_ambient):
-
-    t0=3.51270357e-03
-    t1=1.25173402e-05
-    t2=-2.12813771e-06
-    t3=4.37897948e-08
-
-    return t0 + t1 * am + t2 * am ** 2 + t3 * am ** 3
+    #gt=glass_transmission_losses(aoi)
+    #print(gt)
+    get=pd.DataFrame.from_dict(aoi_list, orient='index')
+    plt.plot(get)
+    plt.show()
