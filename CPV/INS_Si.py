e6b4f73474f773762e1008e4f79067b6de1bdfe3
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-10-25 14:13:05 +0200

    dni corrected

--- CPV/INS_Si.py
+++ CPV/INS_Si.py
@@ -44,8 +44,7 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
     spa_python = pvlib.solarposition.spa_python(time=times, latitude=lat,
                                                 longitude=lon)
     #calculate extraterrestrial radiation for diffuse model
-    extra = pvlib.irradiance.get_extra_radiation(times, solar_constant=1366.1,
-                                      method='spencer', epoch_year=2014)
+    extra = pvlib.irradiance.get_extra_radiation(weather_loc.index)
     extra = extra.to_frame('extra')
     zenith = spa_python[['zenith']].fillna(0)
     azimuth = spa_python[['azimuth']].fillna(0)
@@ -73,7 +72,7 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
         pz[index] = perez_diffuse
 
     perez = pd.DataFrame(pz, columns=["perez"])
-
+    weather_loc['perez_diffuse']=perez
 
     # calculate DNI
     # prepare weather data for AOI >< 60° and change DNI values accordingly
@@ -90,8 +89,10 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
         aoi_list[index] = aoi
         gt_list[index] = gt
 
-    weather_loc['dni-gt']= weather_loc['dni']*gt
 
+    alignement_transmission = 0.95
+    weather_loc['dni']= weather_loc['dni']*gt*alignement_transmission
+    weather_loc['perez_diffuse'] = weather_loc['perez_diffuse'] * gt*alignement_transmission
 
     smallaoi = aoi_list[aoi_list < 60]
     bigaoi = aoi_list[aoi_list > 60]
@@ -101,16 +102,17 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
     weather_bigaoi = weather_loc[weather_loc.index.isin(bigaoi.index)] #todo: adjust DNI for AOI>60°: Durchnitt über Fläche
     weather_data=pd.concat([weather_smallaoi, weather_bigaoi])
     weather_data.sort_index(inplace=True)
+    weather_data
 
     dni = weather_data['dni']
     ground_diffuse = pvlib.irradiance.get_ground_diffuse(surface_tilt=surface_tilt,
                                         ghi=weather_data['ghi'],
                                         albedo=.25, surface_type=None)
 
-    poa_components=pvlib.irradiance.poa_components(aoi= aoi_list, dni=dni, #todo: check if that is needed?
-                                       poa_sky_diffuse=perez['perez'],
+    poa_components=pvlib.irradiance.poa_components(aoi= aoi_list, dni=dni,        #todo: check if that is needed? DNI set to 0!!
+                                       poa_sky_diffuse=weather_loc['perez_diffuse'],
                                         poa_ground_diffuse=ground_diffuse)
-
+    poa_components['poa_diffuse']=weather_loc['perez_diffuse']
     prepared_dict= pd.concat(
         [poa_components['poa_global'], poa_components['poa_direct'], poa_components['poa_diffuse'], aoi_list, absulute_airmass], axis=1)
 
