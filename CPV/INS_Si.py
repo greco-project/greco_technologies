660db08434cb3623a395f0461ad4ec45c42c3db5
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-11-15 15:49:55 +0100

    function create SI timeseries added

--- CPV/INS_Si.py
+++ CPV/INS_Si.py
@@ -4,9 +4,13 @@ import pandas as pd
 import pvlib.solarposition
 import pvlib.atmosphere
 import pvlib.irradiance
+from pvlib.pvsystem import PVSystem
+from pvlib.location import Location
+import pvlib.pvsystem as pvsystem
+import os
 
 import pvlib_CPVsystem as cpv
-
+import matplotlib.pyplot as plt
 
 
 def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth):
@@ -89,7 +93,6 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
         aoi_list[index] = aoi
         gt_list[index] = gt
 
-
     alignement_transmission = 0.95
     weather_loc['dni']= weather_loc['dni']*gt*alignement_transmission
     weather_loc['perez_diffuse'] = weather_loc['perez_diffuse'] * gt*alignement_transmission
@@ -102,7 +105,6 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
     weather_bigaoi = weather_loc[weather_loc.index.isin(bigaoi.index)] #todo: adjust DNI for AOI>60°: Durchnitt über Fläche
     weather_data=pd.concat([weather_smallaoi, weather_bigaoi])
     weather_data.sort_index(inplace=True)
-    weather_data
 
     dni = weather_data['dni']
     ground_diffuse = pvlib.irradiance.get_ground_diffuse(surface_tilt=surface_tilt,
@@ -116,4 +118,75 @@ def INS_Si_prepare_weather(weather_loc, lat, lon, surface_tilt, surface_azimuth)
     prepared_dict= pd.concat(
         [poa_components['poa_global'], poa_components['poa_direct'], poa_components['poa_diffuse'], aoi_list, absulute_airmass], axis=1)
 
+    plt.plot(aoi_list, dni, 'b.', label='DNI')
+    plt.plot(aoi_list, weather_loc['perez_diffuse'], 'g.', label='DHI')
+    plt.xlabel('AOI')
+    plt.ylabel('Irradiance')
+    plt.legend()
+    plt.show()
     return prepared_dict
+
+def create_SI_timeseries(lat, lon, weather, surface_azimuth, surface_tilt):
+
+
+    sandia_modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
+    module_ref = sandia_modules['Canadian_Solar_CS5P_220M___2009_']
+    module_ref['Cells_in_Series'] = 4
+    module_ref['Cells_in_Parallel'] = 1
+    module_ref['Isco'] = 1
+    module_ref['Voco'] = 4
+    module_ref['Impo'] = 6.52
+    module_ref['Vmpo'] = 5
+
+    system_ref = PVSystem(surface_tilt=surface_tilt, surface_azimuth=surface_azimuth,
+                          module_parameters=module_ref,
+                          inverter_parameters=None)
+
+    # prepare dictionary with poa_global, poa_direct_poa_diffuse, absolute_airmass, aoi
+    prepared_poas = INS_Si_prepare_weather(weather, lat=lat,
+                                                  lon=lon, surface_tilt=surface_tilt,
+                                                  surface_azimuth=surface_azimuth)
+
+    # todo: adapt function for effective irradiance to increase diffuse fraction
+    # todo: adjust sapm_spectral_loss(airmass_absolute, module) and sapm_aoi_loss(aoi, module) that are included in the effective_irradiance
+
+    effective_irradiance = pvsystem.sapm_effective_irradiance(
+        poa_direct=prepared_poas['poa_direct'],
+        poa_diffuse=prepared_poas['poa_diffuse'],
+        airmass_absolute=prepared_poas['airmass'],
+        aoi=prepared_poas['aoi'],
+        module=module_ref)
+
+    weather['effective_irrandiance'] = effective_irradiance
+    temp_cell = pvsystem.sapm_celltemp(poa_global=prepared_poas['poa_global'],
+                                       wind_speed=weather['wind_speed'],
+                                       temp_air=weather['temp_air'],
+                                       model='open_rack_cell_glassback')  # todo: adjust model
+    temp_cell = temp_cell.fillna(0)
+    output = pvsystem.sapm(effective_irradiance=effective_irradiance,
+                           temp_cell=temp_cell['temp_cell'],
+                           module=module_ref)
+
+    return output.p_mp
+
+if __name__ == '__main__':
+
+    filename = os.path.abspath(
+        "/home/local/RL-INSTITUT/inia.steinbach/rl-institut/04_Projekte/163_Open_FRED/03-Projektinhalte/AP2 Wetterdaten/open_FRED_TestWetterdaten_csv/fred_data_test_2016.csv")
+    weather_df = pd.read_csv(filename, skiprows=range(1, 50), nrows=(5000),
+                             index_col=0,
+                             date_parser=lambda idx: pd.to_datetime(idx,
+                                                                    utc=True))
+    weather_df.index = pd.to_datetime(weather_df.index).tz_convert(
+        'Europe/Berlin')
+
+
+    coordinates = weather_df.loc[:, ["lat", "lon"]].values
+
+    for lat, lon in coordinates:
+        weather_loc = weather_df.loc[
+            (weather_df['lat'] == lat)  # kann das weg?
+            & (weather_df['lon'] == lon)]
+
+        create_SI_timeseries(lat=52.11113, lon=12.48062, weather=weather_loc, surface_azimuth=180, surface_tilt=30)
+        break
