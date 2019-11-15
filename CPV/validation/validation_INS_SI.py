4622d8571a1353b7cdfc13b21c62cc83e038580a
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-11-15 15:51:46 +0100

    Si module parameters changed

--- CPV/validation/validation_INS_SI.py
+++ CPV/validation/validation_INS_SI.py
@@ -1,15 +1,13 @@
 
 import pandas as pd
-import os
 from pvlib.pvsystem import PVSystem
-from pvlib.location import Location
 import matplotlib.pyplot as plt
-
 import pvlib.atmosphere
 import pvlib.pvsystem as pvsystem
 
+import sys
+sys.path.append('/home/local/RL-INSTITUT/inia.steinbach/Dokumente/greco_technologies_to_pvlib/CPV/')
 import INS_Si
-import visualizing_data
 
 
 df= pd.read_csv('/home/local/RL-INSTITUT/inia.steinbach/rl-institut/04_Projekte/220_GRECO/03-Projektinhalte/AP4_High_Penetration_of_Photovoltaics/T4_3_CPV/INS/MAY/InsolightMay2019_filtered.csv', sep=',', index_col=0)
@@ -19,9 +17,12 @@ df['dhi']=df['ghi']-df['dni']
 
 sandia_modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
 module_ref= sandia_modules['Canadian_Solar_CS5P_220M___2009_']
-module_ref['Cells_in_Series']=1
+module_ref['Cells_in_Series']=4
 module_ref['Cells_in_Parallel']=1
-
+module_ref['Isco']=1
+module_ref['Voco']=4
+module_ref['Impo']=6.52
+module_ref['Vmpo']=5
 
 system_ref = PVSystem(surface_tilt=30, surface_azimuth=180,
                   module_parameters=module_ref,
@@ -35,7 +36,7 @@ prepared_poas= INS_Si.INS_Si_prepare_weather(df, lat=45.641603, lon=-3.727, surf
 # todo: adjust sapm_spectral_loss(airmass_absolute, module) and sapm_aoi_loss(aoi, module) that are included in the effective_irradiance
 
 
-effective_irradiance = pvsystem.sapm_effective_irradiance(poa_direct=0,
+effective_irradiance = pvsystem.sapm_effective_irradiance(poa_direct=prepared_poas['poa_direct'],
                                                     poa_diffuse=prepared_poas['poa_diffuse'],
                                                     airmass_absolute = prepared_poas['airmass'],
                                                     aoi= prepared_poas['aoi'],
@@ -57,10 +58,26 @@ modeled_isc=output.i_sc
 real_power=df['Pmp_Si']
 real_isc=df['Isc_Si']
 
-plt.plot(modeled_power, 'b')
-plt.plot(real_power, 'r')
+plt.plot(prepared_poas['aoi'], effective_irradiance, 'b.', label='effective irradiance')
+#plt.plot(aoi_list, weather_loc['perez_diffuse'], 'g.', label='DHI')
+plt.xlabel('AOI')
+plt.ylabel('Irradiance')
+plt.legend()
 plt.show()
 
-plt.plot(modeled_isc, 'b')
-plt.plot(real_isc, 'r')
+
+plt.plot(modeled_power, 'b', label='modeled power')
+plt.plot(real_power, 'r', label='measured power')
+plt.xlabel('time')
+plt.ylabel('power in W')
+plt.legend()
 plt.show()
+
+plt.plot(prepared_poas['aoi'], modeled_power, 'b.', label='modeled_power')
+plt.plot(prepared_poas['aoi'], real_power, 'r.', label='measured power')
+plt.xlabel('AOI')
+plt.ylabel('power in W')
+plt.legend()
+plt.show()
+
+
