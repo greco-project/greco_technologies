ef9941b4c688e0601e83e118cc0697b4a097b7b9
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-11-15 15:50:19 +0100

    cpv class utilization function updated

--- CPV/pvlib_CPVsystem.py
+++ CPV/pvlib_CPVsystem.py
@@ -191,21 +191,6 @@ class CPVSystem(object):
             ot = c4 + c3 * aoi + c2 * aoi ** 2 + c1 * aoi ** 3 + c0 * aoi ** 4
             return ot
 
-    def uf_aoi(self, aoi):
-
-        c0= 1.00002238e+00
-        c1= -5.69836743e-03
-        c2= 4.26384970e-04
-        c3= -2.01190877e-05
-        c4= 1.76364545e-07
-
-        if aoi >= 60:
-            return 0
-        else:
-            ot = c4 + c3 * aoi + c2 * aoi ** 2 + c1 * aoi ** 3 + c0 * aoi ** 4
-            return ot
-
-
     def glass_transmission_losses(self, aoi):  # todo: find mistake for aoi=59°
 
         # this is an insolight specific anti-reflection coating
@@ -233,67 +218,6 @@ class CPVSystem(object):
         Reff = 0.5 * (Rs + Rp)
         glass_transmission = (1.0 - Reff + glass_ar_offset) ** 2
         return glass_transmission
-
-    def ufam(self, am):
-
-        a0 = 1.0015288
-        a1 = -0.00878182
-        a2 = -0.00146694
-
-        return a0 + a1 * am + a2 * am ** 2
-
-    def ufdni(self, dni):
-
-        d0 = 0
-        d1 = 0
-        d2 = 0
-
-        return d0 + d1 * dni + d2 * dni ** 2
-
-    def uftamb(self, t_ambient):
-
-        t0 = 8.43234532e-01
-        t1 = 1.04128138e-02
-        t2 = -1.73841969e-04
-
-        return t0 + t1 * t_ambient + t2 * t_ambient ** 2
-
-    def UF_corrected_DNI(self, am, t_ambient, dni):
-
-        '''
-        The approach follows the model of Gerstmaier (Quelle), promoting a Utilization Faktor
-        that reflects the spectral (AM), temperature (Tamb) and DNI dependencies of
-        multijunction cells. The Utilization Factor is multiplied with the DNI.
-
-        UF = c1 * UF(AM) + c2 * UF(DNI) + c3 * UF(Tamb)
-
-        Parameters
-        --------------
-        am: float or Series
-            airmass
-        t_ambient: float or Series
-            ambient temperature
-        dni: float or Series
-            direct normal irradiance
-
-
-        :return: The altered DNI that includes the parametrization of multijunction
-                cells
-        '''
-
-        uf_am = ufam(am)
-        uf_dni = ufdni(dni)
-        uf_tamb = uftamb(t_ambient)
-        c1 = 1
-        c2 = 1
-        c3 = 1
-
-        df_new = pd.concat([uf_am, uf_dni, uf_tamb], axis=1)
-        df = df_new.sum(axis=1)
-
-        #    UF = c1 * uf_am + c2 * uf_dni + c3 * uf_tamb
-
-        return dni * df
 
 
 class StaticCPVSystem(CPVSystem):
@@ -433,46 +357,6 @@ def glass_transmission_losses(aoi):  # todo: find mistake for aoi=59°
     glass_transmission = (1.0 - Reff + glass_ar_offset) ** 2
     return glass_transmission
 
-def ufam(am):
-    a0 = 3.14343721e-03
-    a1 = 1.02361039e-05
-    a2 = -8.37260985e-08
-
-    return a0 + a1 * am + a2 * am ** 2
-
-def ufdni(dni):
-
-    if not (dni is None):
-        d0 = 0
-        d1 = 0
-        d2 = 0
-
-        return d0 + d1 * dni + d2 * dni ** 2
-    else:
-        return 0
-
-def uftamb(t_ambient):
-    t0 = 3.18368513e-03
-    t1 = 6.47684709e-06
-
-    return t0 + t1 * t_ambient
-
-
-def uf_aoi(aoi):
-
-    c0 = 1.76364545e-07
-    c1 = -2.01190877e-05
-    c2 = 4.26384970e-04
-    c3 = -5.69836743e-03
-    c4 = 1.00002238e+00
-
-    if aoi >= 60:
-        return 0
-    else:
-        ot = c4 + c3 * aoi + c2 * aoi ** 2 + c1 * aoi ** 3 + c0 * aoi ** 4
-        return ot
-
-
 def get_single_util_factor(x, thld, m_low, m_high):
     """
     Retrieves the utilization factor for a variable.
@@ -540,97 +424,6 @@ def calculate_utilization_factor(uf_am, uf_temp, weight_am, weight_temp, calcula
 
     return UF
 
-########helpers functions################
-
-def fit_sde_sandia(V, I, Voc, Isc, Vmp, Imp, vlim=0.2, ilim=0.1):
-    """ Fits the single diode equation to an IV curve.
-    If fitting fails, returns NaN in each parameter.
-    Parameters
-    ----------
-    V : numeric
-        Voltage at each point on the IV curve, from 0 to Voc
-    I : numeric
-        Current at each point on the IV curve, from Isc to 0
-    Voc : float
-        Open circuit voltage
-    Isc : float
-        Short circuit current
-    Vmp : float
-        Voltage at maximum power point
-    Imp : float
-        Current at maximum power point
-    vlim : float, default 0.2
-        defines linear portion of IV curve i.e. V <= vlim * Voc
-    ilim : float, default 0.1
-        defines exponential portion of IV curve i.e. I > ilim * Isc
-    Returns
-    -------
-    IL : float
-        photocurrent, A
-    I0 : float
-        dark (saturation) current, A
-    Rs : float
-        series resistance, ohm
-    Rsh : float
-        shunt (parallel) resistance, ohm
-    nNsVth : float
-        product of diode (ideality) factor n (unitless) x number of
-        cells in series Ns (unitless) x cell thermal voltage Vth (V), V
-    References
-    ----------
-    [1] C. B. Jones, C. W. Hansen, Single Diode Parameter Extraction from
-    In-Field Photovoltaic I-V Curves on a Single Board Computer, 46th IEEE
-    Photovoltaic Specialist Conference, Chicago, IL, 2019
-    """
-    # Find intercept and slope of linear portion of IV curve.
-    # Start with V < vlim * Voc, extend by adding points until slope is
-    # acceptable
-    beta = [np.nan for i in range(5)]
-    beta[0] = np.nan
-    beta[1] = np.nan
-    idx = len(V <= vlim * Voc)
-    while np.isnan(beta[1]) and (idx<=len(V)):
-        try:
-            p = np.polyfit(V[:idx], I[:idx], deg=1)
-            if p[1] < 0:
-                beta[0] = p[0]
-                beta[1] = -p[1] # sign change to get positive parameter value
-        except:
-            pass
-        if np.isnan(beta[1]):
-            idx += 1
-
-    if not np.isnan(beta[0]):
-        # Find parameters from exponential portion of IV curve
-        Y = beta[0] - beta[1] * V - I
-        X = np.array([V, I])
-        idx = len(Y <= ilim * Isc)
-        try:
-            p = np.linalg.lstsq(X, Y)
-            beta[3] = p[1]
-            beta[4] = p[2]
-        except:
-            pass
-
-    if not any([np.isnan(beta[i]) for i in [0, 1, 3, 4]]):
-        # calculate parameters
-        nNsVth = 1.0 / beta[3]
-        Rs = beta[4] / beta[3]
-        Gp = beta[1] / (1.0 - Rs * beta[1])
-        Rsh = 1.0 / Gp
-        IL = (1 + Gp * Rs) * beta[0]
-        # calculate I0
-        I0_Vmp = _calc_I0(IL, Imp, Vmp, Gp, Rs, beta[3])
-        I0_Voc = _calc_I0(IL, 0, Voc, Gp, Rs, beta[3])
-        if (I0_Vmp > 0) and (I0_Voc > 0):
-            I0 = 0.5 * (I0_Vmp + I0_Voc)
-        elif (I0_Vmp > 0):
-            I0 = I0_Vmp
-        elif (I0_Voc > 0):
-            I0 = I0_Voc
-        else:
-            I0 = np.nan
-    return IL, I0, Rs, Rsh, nNsVth
 
 
 
