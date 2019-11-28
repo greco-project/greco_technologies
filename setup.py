37df53071a30680662e4bf23ea4f4afbdaa47123
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2019-11-28 16:08:05 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='greco_technologies',
+    author='Inia Steinbach',
+    author_email='inia.steinbach@rl-institut.de',
+    description='This repository contains modules for modeling the greco '
+                'PV technologies.',
+    namespace_package=['greco_technologies'],
+    long_description=read('README.md'),
+    packages=find_packages(),
+    package_dir={'greco_technologies': 'greco_technologies'},
+    extras_require={
+          'dev': ['sphinx', 'sphinx_rtd_theme', 'requests']},
+    install_requires=[
+        'pvlib >= 0.6.3',
+        'pandas >= 0.24.1',
+        'numpy',
+        'matplotlib'])
