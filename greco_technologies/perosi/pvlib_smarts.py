339e0fb91a003eaa664e3590dab9d670c292b30d
adminlocal <adminlocal@rli-nb-18.rl-institut.local>
2020-08-17 11:12:58 +0200

    undo exception in detail

--- greco_technologies/perosi/pvlib_smarts.py
+++ greco_technologies/perosi/pvlib_smarts.py
@@ -1033,14 +1033,14 @@ def _smartsAll(
     ## Read SMARTS 2.9.5 Output File
     open_csv = os.path.join(file_directory, "smarts295.ext.txt")
 
-    try:
-        data = pd.read_csv(open_csv, delim_whitespace=True)
-    except Exception as e:
-        print(
-            f"the spectrum is empty."
-        )
-        print(e)
-        data = pd.DataFrame()
+    data = pd.read_csv(open_csv, delim_whitespace=True)
+    # try:
+    #     data = pd.read_csv(open_csv, delim_whitespace=True)
+    # except:
+    #     print(
+    #         f"the spectrum is empty."
+    #     )
+    #     data = pd.DataFrame()
 
     try:
         os.remove(os.path.join(file_directory, "smarts295.inp.txt"))
